159223c06921eaafa6a2fa062e20e8c530d2879d
Web-Mustafiz <mustafiz.kst2020@gmail.com>
2022-11-23 14:34:02 +0600

    mondodb insert Oparation done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,33 +8,55 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// creating user data
-const users = [
-  { id: 1, name: "Babar Azam", email: "bazam@gmail.com" },
-  { id: 2, name: "Rizwan", email: "rizwan@gmail.com" },
-  { id: 3, name: "Khushdil Sah", email: "kshah@gmail.com" },
-  { id: 4, name: "Asif Ali", email: "aali@gmail.com" },
-];
-app.get("/users", (req, res) => {
-  if (req.query.name) {
-    const search = req.query.name;
-    const filtered = users.filter(
-      (usr) => usr.name.toLowerCase().indexOf(search) >= 0
-    );
-    res.send(filtered);
-  } else {
-    res.send(users);
-  }
-});
+const uri =
+  "mongodb+srv://Admin102:RkLynzN6OLdIIvLN@cluster0.cnotlwz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
-app.post("/users", (req, res) => {
-  console.log("Post API Hit...");
-  const user = req.body;
-  user.id = users.length + 1;
-  users.push(user);
-  console.log(user);
-  res.send(user);
-});
+async function run() {
+  try {
+    const userCollection = client.db("simpledata").collection("users");
+
+    // Get the user Data by the Database
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    // create a document to insert
+    app.post("/users", async (req, res) => {
+      console.log("Post API Hit...");
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      user.id = result.insertedId;
+      res.send(user);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// app.get("/users", (req, res) => {
+//   if (req.query.name) {
+//     const search = req.query.name;
+//     const filtered = users.filter(
+//       (usr) => usr.name.toLowerCase().indexOf(search) >= 0
+//     );
+//     res.send(filtered);
+//   } else {
+//     res.send(users);
+//   }
+// });
+
+// app.post("/users", (req, res) => {
+//   console.log("Post API Hit...");
+//   const user = req.body;
+//   user.id = users.length + 1;
+//   users.push(user);
+//   console.log(user);
+//   res.send(user);
+// });
 
 app.listen(port, () => {
   console.log(`Data Server is Runnig on Port ${port}`);
